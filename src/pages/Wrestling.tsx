ba7d3acbfef11ea208610db10aa8b3613d2aabd5
Sathiya31 <35345872+Sathiya31@users.noreply.github.com>
2025-09-01 23:07:09 +0530

    Add Wrestling page and calendar component with UWW events JSON

---
+++ src/pages/Wrestling.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import WrestlingCalendar from '../components/WrestlingCalendar';
+
+const Wrestling: React.FC = () => (
+  <div>
+    <h1>Wrestling Events</h1>
+    <WrestlingCalendar />
+  </div>
+);
+
+export default Wrestling;
